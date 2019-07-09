f7bcf35b5534ad718303e88a6d833d0cb60d2ae7
Ndelforno <nicolas.delforno@gmail.com>
2019-07-08 20:20:14 -0400

    axios request created to get xuid

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,27 @@
+console.log('test');
+
 const fetchXboxProfile = (query) => {
   return axios({
-    url: "https://xboxapi.com/v2/profile",
+    url: "https://xboxapi.com/v2/xuid/" + query,
     method: 'get',
     data: '',
     dataType: 'json',
     headers: {
-      X-AUTH:"91d05ba9314a8674642feacc02bd3ab3af4e1d6d"
+      "X-Auth" : "91d05ba9314a8674642feacc02bd3ab3af4e1d6d",
     }
   });
+};
 
   document.addEventListener("turbolinks:load", async (event) =>{
-    const hasUsername = document.getElementById('username')
+    console.log("test");
+    const hasUserName = document.getElementById('username').innerText;
+    console.log(hasUserName)
+    if (hasUserName) {
+      const {
+        data
+      } = await fetchXboxProfile(hasUserName)
+
+      var xuid = data["xuid"]
+      console.log(xuid);
+    }
   })
-};
