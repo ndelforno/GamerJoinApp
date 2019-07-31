69473e3d4019ab725d3821cd3de00f29a30d9705
Ndelforno <nicolas.delforno@gmail.com>
2019-07-31 19:56:55 -0400

    Httparty installed and request made on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    url = "https://xboxapi.com/v2/xuid/#{@user.username}"
+    print url
+    response = HTTParty.get(url, :headers {"X-Auth" => "91d05ba9314a8674642feacc02bd3ab3af4e1d6d"})
+    print response
+    body = JSON.parse(response.body)
   end
 end
