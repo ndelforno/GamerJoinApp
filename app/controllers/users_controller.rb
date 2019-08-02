4c22e11c58b5f2f726b4eac3eacf75ace323c409
Ndelforno <nicolas.delforno@gmail.com>
2019-08-02 16:30:24 -0400

    function to get xbox one games created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,31 @@ class UsersController < ApplicationController
   @@token = "91d05ba9314a8674642feacc02bd3ab3af4e1d6d"
   def show
     @user = User.find(params[:id])
-    @xuid = getxuid(@user)
-    @xboxprofile = getxboxprofile(@xuid)
+    @xuid = get_xuid(@user)
+    @xboxprofile = get_xbox_profile(@xuid)
+    @xboxonegames = get_xbox_one_games(@xuid)
   end
 
-  def getxuid(user)
+  def get_xuid(user)
     url = "https://xboxapi.com/v2/xuid/#{@user.username}"
-    puts url
     response = HTTParty.get(url, headers: {"X-Auth" => @@token })
-    puts response
     body = JSON.parse(response.body)
     return body
   end
 
 
-  def getxboxprofile(xuid)
+  def get_xbox_profile(xuid)
       url = "https://xboxapi.com/v2/#{xuid}/profile"
-      puts url
       response = HTTParty.get(url, headers: {"X-Auth" => @@token})
-      puts response
       body = JSON.parse(response.body)
       return body
+  end
+
+  def get_xbox_one_games(xuid)
+    url = "https://xboxapi.com/v2/#{xuid}/xboxonegames"
+    response = HTTParty.get(url, headers: {"X-Auth" => @@token})
+    body = JSON.parse(response.body)
+    return body
   end
 
 end
