95db299d8157db1170688f64aeee5d9dc596a5bd
Ndelforno <nicolas.delforno@gmail.com>
2019-08-02 16:04:40 -0400

    change api logic from axios to httparty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
-
+  @@token = "91d05ba9314a8674642feacc02bd3ab3af4e1d6d"
   def show
     @user = User.find(params[:id])
-
-    url = "https://xboxapi.com/v2/xuid/#{@user.username}"
-    print url
-    response = HTTParty.get(url, :headers {"X-Auth" => "91d05ba9314a8674642feacc02bd3ab3af4e1d6d"})
-    print response
-    body = JSON.parse(response.body)
+    @xuid = getxuid(@user)
+    @xboxprofile = getxboxprofile(@xuid)
   end
+
+  def getxuid(user)
+    url = "https://xboxapi.com/v2/xuid/#{@user.username}"
+    puts url
+    response = HTTParty.get(url, headers: {"X-Auth" => @@token })
+    puts response
+    body = JSON.parse(response.body)
+    return body
+  end
+
+
+  def getxboxprofile(xuid)
+      url = "https://xboxapi.com/v2/#{xuid}/profile"
+      puts url
+      response = HTTParty.get(url, headers: {"X-Auth" => @@token})
+      puts response
+      body = JSON.parse(response.body)
+      return body
+  end
+
 end
